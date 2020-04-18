2a9724dc901342ebc0d2f61bff01c0b2d4a6d32b
David <sourcecode@mailbot.com>
2020-04-18 10:12:29 +0100

    longest palindrome - simplify code

--- 005-longest-palindrome/main.go
+++ 005-longest-palindrome/main.go
@@ -8,9 +8,6 @@ func longestPalindrome(s string) string {
 
 	// each element represents the length of the palindrome at that index in the string
 	ps := make([]int, len(s), len(s))
-	for i, _ := range ps {
-		ps[i] = 1 // every letter is a palindrome of itself
-	}
 
 	var longestIdx int
 	for i, _ := range s {
@@ -23,19 +20,20 @@ func longestPalindrome(s string) string {
 			}
 
 			for ; 0 <= start && end < len(s); start, end = start-1, end+1 {
-				if s[start] != s[end] {
+				// not a palindrome or
+				// previously seen palindrome that is longer that current palindrome, so short circuit
+				if s[start] != s[end] || ps[start] > end-start {
 					break
 				}
-				if ps[start] > (end-start)+1 {
-					// previously seen palindrome that is longer that current palindrome, so short circuit
-					break
-				}
-				ps[start] = (end - start) + 1
+				ps[start] = end - start
 				if ps[start] > ps[longestIdx] {
 					longestIdx = start
 				}
 			}
 		}
 	}
-	return string(s[longestIdx : longestIdx+ps[longestIdx]])
+	if ps[longestIdx] == 0 {
+		return s[:1] // if longest length is zero, then all letters are unique
+	}
+	return s[longestIdx : longestIdx+ps[longestIdx]+1]
 }
