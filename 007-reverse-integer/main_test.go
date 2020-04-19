380bf2deac4ac0fce79ca5f6f00e62c4c25a7489
David <sourcecode@mailbot.com>
2020-04-19 20:40:18 +0100

    reverse-integer - bootstrap

---
+++ 007-reverse-integer/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import "testing"
+
+func Test_reverse(t *testing.T) {
+	type args struct {
+		x int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{"example 1", args{123}, 321},
+		{"example 2", args{-123}, -321},
+		{"example 3", args{120}, 12},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := reverse(tt.args.x); got != tt.want {
+				t.Errorf("reverse() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
