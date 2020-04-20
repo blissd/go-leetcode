0dba3dfe7472b2e084216aa1265db8ee4ccfcbb4
David <sourcecode@mailbot.com>
2020-04-20 12:38:55 +0100

    string-to-int - solution

--- 008-string-to-number/main.go
+++ 008-string-to-number/main.go
@@ -1,5 +1,46 @@
 package main
 
-func myAtoi(str string) int {
-	return 0
+import (
+	"math"
+	"unicode"
+)
+
+func myAtoi(s string) int {
+	if s == "" {
+		return 0
+	}
+	var start int
+	for i, c := range s {
+		if unicode.IsSpace(c) {
+			continue
+		}
+		if unicode.IsNumber(c) || c == '-' || c == '+' {
+			start = i
+			break
+		}
+		return 0
+	}
+	neg := s[start] == '-'
+	if s[start] == '-' || s[start] == '+' {
+		start += 1
+	}
+
+	var sum int
+	for _, c := range s[start:] {
+		if !unicode.IsNumber(c) {
+			break
+		}
+		sum *= 10
+		sum += int(c - '0')
+
+		if neg && sum > math.MaxInt32+1 {
+			return math.MinInt32
+		} else if !neg && sum > math.MaxInt32 {
+			return math.MaxInt32
+		}
+	}
+	if neg {
+		return -sum
+	}
+	return sum
 }
--- 008-string-to-number/main_test.go
+++ 008-string-to-number/main_test.go
@@ -15,6 +15,10 @@ func Test_myAtoi(t *testing.T) {
 		{"example 2", args{"   -42"}, -42},
 		{"example 3", args{"4193 with words"}, 4193},
 		{"example 4", args{"words and 987"}, 0},
+		{"failure 1", args{"-91283472332"}, -2147483648},
+		{"failure 2", args{"3.14159"}, 3},
+		{"failure 3", args{"+1"}, 1},
+		{"failure 4", args{"-2147483648"}, -2147483648},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
