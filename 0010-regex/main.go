6daba5fab498527f045f46195e273090b3ebfb7b
David <sourcecode@mailbot.com>
2020-04-21 12:30:15 +0100

    regex still failing - wip

--- 0010-regex/main.go
+++ 0010-regex/main.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
 type result int
 
 const (
@@ -20,13 +16,11 @@ const (
 // a rule for matching a rune
 type matcher interface {
 	// is this rune matched? Nil returned if matched.
-	match(c rune) result
-	// error for when matcher isn't evaluated against any runes. Nil for no error.
-	unused() error
+	match(c uint8) result
 }
 
 // Matches a specific rune.
-type exact rune
+type exact uint8
 
 // Matches any rune.
 type any struct{}
@@ -36,34 +30,22 @@ type repeat struct {
 	m matcher
 }
 
-func (m exact) match(c rune) result {
-	if c == rune(m) {
+func (m exact) match(c uint8) result {
+	if c == uint8(m) {
 		return matched
 	}
 	return fail
 }
 
-func (m exact) unused() error {
-	return fmt.Errorf("unmatched rune: %v", rune(m))
-}
-
-func (m any) match(_ rune) result {
+func (m any) match(_ uint8) result {
 	return matched
 }
 
-func (m any) unused() error {
-	return nil
-}
-
-func (m repeat) match(c rune) result {
+func (m repeat) match(c uint8) result {
 	if m.m.match(c) != fail {
 		return matched_repeat
 	}
 	return unmatched
-}
-
-func (m repeat) unused() error {
-	return nil
 }
 
 func compile(p string) []matcher {
@@ -98,57 +80,43 @@ func compile(p string) []matcher {
 func isMatch(s string, p string) bool {
 
 	matchers := compile(p)
-	var m int
-	for i, r := range s {
-		i = i
-		if m == len(matchers) {
-			// we've used the last matcher but still have more characters to evaluate, so fail
-			return false
-		}
-
-		result := matchers[m].match(r)
-
+	var i, m int
+	for m < len(matchers) && i < len(s) {
+		//if i == len(s) {
+		//	return false // unused matchers remaining
+		//}
+		mm := matchers[m]
+		result := mm.match(s[i])
 		if result == fail {
 			return false
+		} else if result == unmatched {
+			m++
+			continue
 		} else if result == matched {
 			m++
-		} else if result == unmatched {
-			// no match, so advance matchers until match or failure
-			for m += 1; m < len(matchers); m++ {
-				result := matchers[m].match(r)
-				if result == fail {
-					return false
-				}
-				if result == unmatched {
+			i++
+		} else if result == matched_repeat {
+			// advance matcher
+			for j := m + 1; j < len(matchers); j++ {
+				if matchers[j].match(s[i]) == matched_repeat {
+					m = j
 					continue
 				}
-				if result == matched_repeat {
-					break
-				}
-				if result == matched {
-					m++
-					break
-				}
+				break
 			}
-		} else if result == matched_repeat {
-			// must match shortest set of repeating characters
-			for mm := m + 1; mm < len(matchers); mm++ {
-				if matchers[mm].match(r) != matched_repeat {
-					break
-				}
-				m = mm
-			}
+			i++
+			//for j := i+1; j < len(s) && (mm.match(rune(s[i])) == matched_repeat||mm.match(rune(s[i])) == unmatched); i++ {
+			//}
 		}
 	}
 
-	// test matchers to see if they produce an error when they are unused
-	if m < len(matchers) {
-		for ; m < len(matchers); m++ {
-			if matchers[m].unused() != nil {
-				return false
-			}
+	if i != len(s) {
+		return false
+	}
+	for ; m < len(matchers); m++ {
+		if _, ok := matchers[m].(exact); ok {
+			return false
 		}
 	}
-
-	return true
+	return i == len(s)
 }
