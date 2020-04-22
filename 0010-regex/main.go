d15030ae679bf1f7e96cd9f8bdb34a7c19f8bf24
David <sourcecode@mailbot.com>
2020-04-22 06:38:09 +0100

    regex recursive approach that doesn't quite work

--- 0010-regex/main.go
+++ 0010-regex/main.go
@@ -27,9 +27,6 @@ type composite struct {
 // Matches another matcher zero or more times
 type repeat composite
 
-// match any character except one
-type invert composite
-
 // match exactly
 type exact composite
 
@@ -46,17 +43,6 @@ func (m once) match(c uint8) result {
 
 func (m any) match(_ uint8) result {
 	return advance_letter | advance_matcher
-}
-
-func (m invert) match(c uint8) result {
-	r := m.m.match(c)
-	if r&advance_letter == advance_letter {
-		return advance_matcher
-	}
-	if r&advance_matcher == advance_matcher {
-		return advance_letter
-	}
-	return abort
 }
 
 func (m repeat) match(c uint8) result {
@@ -76,15 +62,15 @@ func compile(p string) []matcher {
 		case r == '*':
 			m := repeat{matchers[len(matchers)-1]}
 			// Consolidate adjacent repeating matchers of the same character
-			if len(matchers) > 2 && matchers[len(matchers)-2] == m {
-				continue
-			}
+			//if len(matchers) > 2 && matchers[len(matchers)-2] == m {
+			//	continue
+			//}
 			// Repeat previous match expression, not previous character
 			matchers[len(matchers)-1] = m
 		default:
-			if len(matchers) > 1 && matchers[len(matchers)-1] == (repeat{exact{any{}}}) {
-				matchers[len(matchers)-1] = invert{repeat{once(r)}}
-			}
+			//if len(matchers) > 1 && matchers[len(matchers)-1] == (repeat{exact{any{}}}) {
+			//	matchers[len(matchers)-1] = invert{repeat{once(r)}}
+			//}
 			//if len(matchers) > 1 {
 			//switch matchers[len(matchers)-1].(type) {
 			//case any:
@@ -96,56 +82,52 @@ func compile(p string) []matcher {
 	}
 
 	// re-order matchers so an exact(x) and repeat(exact(x)) are adjacent, then the exact comes first
-	for i := 1; i < len(matchers); i++ {
-		if matchers[i-1] == (repeat{matchers[i]}) {
-			matchers[i-1], matchers[i] = matchers[i], matchers[i-1]
-			i = 0 // restart sort from beginning
-		}
-	}
+	//for i := 1; i < len(matchers); i++ {
+	//	if matchers[i-1] == (repeat{matchers[i]}) {
+	//		matchers[i-1], matchers[i] = matchers[i], matchers[i-1]
+	//		i = 0 // restart sort from beginning
+	//	}
+	//}
 
 	return matchers
 }
 
-func isMatch(s string, p string) bool {
+func isMatch2(s string, matchers []matcher) bool {
 
-	matchers := compile(p)
+	//if len(s) == 0 || len(matchers) == 0 {
+	//	return false
+	//}
 
-	var exactCount int // count of exact matches in pattern
-	for m := 0; m < len(matchers); m++ {
-		if _, ok := matchers[m].(exact); ok {
-			exactCount++
-		}
-	}
 	var i, m int
 	for m < len(matchers) && i < len(s) {
-		if _, ok := matchers[m].(exact); ok {
-			exactCount--
-		} else if i+exactCount == len(s) {
-			// we are on a repeating matcher but have unused exact matchers.
-			// Advance to next exact matcher and start using that.
-			for ; m < len(matchers); m++ {
-				if _, ok := matchers[m].(exact); ok {
-					break
-				}
-			}
-		}
 		mm := matchers[m]
 		result := mm.match(s[i])
 		if result == abort {
 			return false
 		}
+		if result == advance_matcher|advance_letter {
+			// exact single letter match
+			m++
+			i++
+			continue
+		}
+		if result&advance_letter == advance_letter {
+			// Letter is advancing, but matcher is not.
+			// This means we are evaluating a repeat matcher so we must see if
+			// the subsequent matchers can fully match the string from this position
+			if m+1 < len(matchers) && isMatch2(s[i:], matchers[m+1:]) {
+				return true
+			}
+			i++
+		}
 		if result&advance_matcher == advance_matcher {
 			m++
 		}
-		if result&advance_letter == advance_letter {
-			i++
-		}
 	}
 
-	for ; m < len(matchers); m++ {
-		if _, ok := matchers[m].(exact); ok {
-			return false
-		}
-	}
-	return i == len(s)
+	return (m == len(matchers) || m == len(matchers)-1) && i == len(s)
+}
+
+func isMatch(s string, p string) bool {
+	return isMatch2(s, compile(p))
 }
