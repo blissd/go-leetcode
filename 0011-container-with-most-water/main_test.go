63d3df10d202795dc74d92825146ef0660f98d4e
David <sourcecode@mailbot.com>
2020-04-23 18:12:37 +0100

    water more test cases

--- 0011-container-with-most-water/main_test.go
+++ 0011-container-with-most-water/main_test.go
@@ -12,6 +12,11 @@ func Test_maxArea(t *testing.T) {
 		want int
 	}{
 		{"example 1", args{[]int{1, 8, 6, 2, 5, 4, 8, 3, 7}}, 49},
+		{"failure 1", args{[]int{1, 2}}, 1},
+		{"random 1", args{[]int{1, 2, 3}}, 2},
+		{"random 2", args{[]int{27, 46, 90, 70, 48, 39, 61, 4, 55, 75, 67, 72, 30, 40, 25, 2, 6, 89, 8, 1}}, 1335},
+		{"random 3", args{[]int{10, 78, 52, 2, 5, 48, 49, 25}}, 245},
+		{"random 4", args{[]int{79, 88, 99, 30, 2, 99, 55, 15}}, 395},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
