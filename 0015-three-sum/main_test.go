dc7f7ad32ae6287af50fda8215fc2ea9ff24d800
David <sourcecode@mailbolt.com>
2020-04-27 20:21:28 +0100

    three-sum add failure case

--- 0015-three-sum/main_test.go
+++ 0015-three-sum/main_test.go
@@ -15,6 +15,7 @@ func Test_threeSum(t *testing.T) {
 		want [][]int
 	}{
 		{"example 1", args{[]int{-1, 0, 1, 2, -1, -4}}, [][]int{{-1, 0, 1}, {-1, -1, 2}}},
+		{"failure 1", args{[]int{0, 0}}, [][]int{}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
