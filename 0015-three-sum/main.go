81d2c6d31c71c26c80bdc701f622760215dc9ef1
David <sourcecode@mailbolt.com>
2020-04-28 18:33:25 +0100

    three-sum use multi-return, but still broken

--- 0015-three-sum/main.go
+++ 0015-three-sum/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "sort"
+import (
+	"errors"
+	"sort"
+)
+
+var ErrNoSum = errors.New("no sum")
 
 type three struct {
 	a, b, c int
@@ -9,9 +14,8 @@ type three struct {
 func threeSum(nums []int) [][]int {
 	rs := make(map[three]bool)
 	for _, n := range nums {
-		r := twoSum(nums, -n)
-		if r != nil {
-			r = append(r, n)
+		if a, b, err := twoSum(nums, -1); err == nil {
+			r := []int{a, b, n}
 			sort.Ints(r)
 			rs[three{r[0], r[1], r[2]}] = true
 		}
@@ -24,18 +28,27 @@ func threeSum(nums []int) [][]int {
 	return keys
 }
 
-func twoSum(nums []int, target int) []int {
+func twoSum(nums []int, target int) (int, int, error) {
 	// map from value to index
-	lookup := make(map[int]int)
+	lookup := count(nums)
 	for i, n := range nums {
 		// compute value we want to lookup index for
 		nn := target - n
 		if ii, ok := lookup[nn]; ok && ii != i {
-			return []int{nums[ii], nums[i]} // lower index first
+			return nums[ii], nums[i], nil
 		}
 
 		lookup[n] = i
 	}
 
-	return nil
+	return 0, 0, ErrNoSum
+}
+
+func count(nums []int) map[int]int {
+	counts := make(map[int]int)
+	for _, n := range nums {
+		c, _ := counts[n]
+		counts[n] = c + 1
+	}
+	return counts
 }
--- 0015-three-sum/main_test.go
+++ 0015-three-sum/main_test.go
@@ -16,6 +16,7 @@ func Test_threeSum(t *testing.T) {
 	}{
 		{"example 1", args{[]int{-1, 0, 1, 2, -1, -4}}, [][]int{{-1, 0, 1}, {-1, -1, 2}}},
 		{"failure 1", args{[]int{0, 0}}, [][]int{}},
+		{"failure 2", args{[]int{1, 2, -2, -1}}, [][]int{}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
