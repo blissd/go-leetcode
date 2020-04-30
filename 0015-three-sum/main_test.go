7e3298eb8a48632ef07da2127416661f67895cef
David <sourcecode@mailbolt.com>
2020-04-30 16:52:02 +0100

    three-sum make tests non-flaky

--- 0015-three-sum/main_test.go
+++ 0015-three-sum/main_test.go
@@ -2,8 +2,32 @@ package main
 
 import (
 	"reflect"
+	"sort"
 	"testing"
 )
+
+type threeList [][]int
+
+func (t threeList) Len() int {
+	return len(t)
+}
+
+func (t threeList) Less(i, j int) bool {
+	if t[i][0] < t[j][0] {
+		return true
+	}
+	if t[i][0] == t[j][0] && t[i][1] < t[j][1] {
+		return true
+	}
+	if t[i][0] == t[j][0] && t[i][1] == t[j][1] && t[i][2] < t[j][2] {
+		return true
+	}
+	return false
+}
+
+func (t threeList) Swap(i, j int) {
+	t[i], t[j] = t[j], t[i]
+}
 
 func Test_threeSum(t *testing.T) {
 	type args struct {
@@ -19,9 +43,14 @@ func Test_threeSum(t *testing.T) {
 		{"failure 2", args{[]int{1, 2, -2, -1}}, [][]int{}},
 		{"failure 3", args{[]int{-4, -2, 1, -5, -4, -4, 4, -2, 0, 4, 0, -2, 3, 1, -5, 0}}, [][]int{{-5, 1, 4}, {-4, 0, 4}, {-4, 1, 3}, {-2, -2, 4}, {-2, 1, 1}, {0, 0, 0}}},
 	}
+
+	// sort wanted results
 	for _, tt := range tests {
+		sort.Sort(threeList(tt.want))
 		t.Run(tt.name, func(t *testing.T) {
-			if got := threeSum(tt.args.nums); !reflect.DeepEqual(got, tt.want) {
+			got := threeSum(tt.args.nums)
+			sort.Sort(threeList(got))
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("threeSum() = %v, want %v", got, tt.want)
 			}
 		})
