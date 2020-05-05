d90c32fbd4897665eaed914a4ebc5cfc6bc07360
David <sourcecode@mailbolt.com>
2020-05-05 06:31:10 +0100

    three-sum-closest failure

--- 0016-three-sum-closest/main.go
+++ 0016-three-sum-closest/main.go
@@ -1,5 +1,40 @@
 package main
 
+import (
+	"sort"
+)
+
+func abs(i int) int {
+	if i < 0 {
+		return -i
+	}
+	return i
+}
+
 func threeSumClosest(nums []int, target int) int {
-	return 0
+	sort.Ints(nums)
+	closest := -target
+	for i := 0; i < len(nums)-2; i++ {
+		a := nums[i]
+		start := i + 1
+		end := len(nums) - 1
+		for start != end {
+			b := nums[start]
+			c := nums[end]
+			sum := a + b + c
+			if target == sum {
+				return sum
+			}
+			if abs(target-sum) < abs(target-closest) {
+				closest = sum
+			}
+			if target-sum > 0 {
+				end--
+			} else {
+				start++
+			}
+		}
+	}
+
+	return closest
 }
--- 0016-three-sum-closest/main_test.go
+++ 0016-three-sum-closest/main_test.go
@@ -12,7 +12,9 @@ func Test_threeSumClosest(t *testing.T) {
 		args args
 		want int
 	}{
-		{"example 2", args{[]int{-1, 2, 1, -4}, 1}, 2},
+		{"example 1", args{[]int{-1, 2, 1, -4}, 1}, 2},
+		{"failure 1", args{[]int{1, 1, 1, 0}, 100}, 3},
+		{"failure 2", args{[]int{0, 1, 2}, 0}, 3},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
