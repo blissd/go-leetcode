fb57e3672d437616397aa212bf34f58889dd3877
David <sourcecode@mailbolt.com>
2020-06-11 18:31:10 +0100

    valid-parens passing tests

--- 0020-valid-parens/main.go
+++ 0020-valid-parens/main.go
@@ -1,5 +1,43 @@
 package main
 
+type stack []rune
+
+func (s stack) push(r rune) stack {
+	return append(s, r)
+}
+
+func (s stack) pop() stack {
+	return s[:len(s)-1]
+}
+
+func (s stack) empty() bool {
+	return len(s) == 0
+}
+
+func (s stack) peek() rune {
+	return s[len(s)-1]
+}
+
 func isValid(s string) bool {
-	return false
+	var ss stack = make([]rune, 0, 0)
+	for _, r := range s {
+		if r == '{' || r == '(' || r == '[' {
+			ss = ss.push(r)
+		} else if r == '}' || r == ')' || r == ']' {
+			if ss.empty() {
+				return false
+			}
+			if r == '}' && ss.peek() != '{' {
+				return false
+			}
+			if r == ']' && ss.peek() != '[' {
+				return false
+			}
+			if r == ')' && ss.peek() != '(' {
+				return false
+			}
+			ss = ss.pop()
+		}
+	}
+	return ss.empty()
 }
