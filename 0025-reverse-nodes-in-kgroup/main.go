18cd13da39a32407b6d561610ceac2bc3a28e475
David <sourcecode@mailbolt.com>
2020-06-26 19:05:11 +0100

    reverse-nodes-in-kgroup first cut

--- 0025-reverse-nodes-in-kgroup/main.go
+++ 0025-reverse-nodes-in-kgroup/main.go
@@ -1,5 +1,40 @@
 package main
 
-func reverseKGroup(head *ListNode, k int) *ListNode {
+func atLeast(head *ListNode, n int) bool {
+	for i := 0; i < n; head, i = head.Next, i+1 {
+		if head == nil {
+			return false
+		}
+	}
+	return true
+}
 
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	if !atLeast(head, k) {
+		return head
+	}
+
+	var first ListNode
+	last := &first
+
+	for atLeast(head, k) {
+		// build reversed segment
+		end := &ListNode{head.Val, nil}
+		start := end
+		head = head.Next
+		for i := 0; i < k-1; i++ {
+			start = &ListNode{head.Val, start}
+			head = head.Next
+		}
+
+		last.Next = start
+		last = end
+	}
+
+	last.Next = head
+	return &first
 }
